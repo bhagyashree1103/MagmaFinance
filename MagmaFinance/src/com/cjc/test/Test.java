c1752a590412034725af800fad00a7d4c99ff680
BHAGYASHREE <bhagyashreedreams11@gmail.com>
2020-04-13 21:26:00 +0530

    test class modified

--- MagmaFinance/src/com/cjc/test/Test.java
+++ MagmaFinance/src/com/cjc/test/Test.java
@@ -3,5 +3,21 @@ package com.cjc.test;
 public class Test {
 public static void main(String[] args) {
 System.out.println("welcome");
+
+
+int a=10;
+//primitive to object
+
+Integer i= new Integer(a);
+
+System.out.println(i);
+
+i= Integer.valueOf(a);
+
+System.out.println(i);
+
+
+int j=i;
+System.out.println(j);
 }
 }
